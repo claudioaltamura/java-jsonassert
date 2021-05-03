7403c88fba45f1c052d46fdd002b669d553eb5f8
Claudio Altamura <claudio@claudioaltamura.de>
2021-05-03 14:48:05 +0200

    chore: upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,22 +4,22 @@ plugins {
     java
     eclipse
     idea
-    id("com.diffplug.gradle.spotless") version "3.27.1"
+    id("com.diffplug.spotless") version "5.12.4"
+    id("com.github.ben-manes.versions") version "0.38.0"
 }
 
 repositories {
-    jcenter()
+    mavenCentral()
 }
 
 dependencies {
-    implementation("com.google.guava:guava:28.0-jre")
-    implementation("com.fasterxml.jackson.core:jackson-core:2.11.1")
-    implementation("com.fasterxml.jackson.core:jackson-annotations:2.11.1")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.11.1")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.12.3")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.12.3")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.3")
 
     testImplementation("org.skyscreamer:jsonassert:1.5.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.1")
-
+    testImplementation("org.assertj:assertj-core:3.19.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.1")
 }
 
