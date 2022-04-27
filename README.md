89c5c1fbb80bda0df09822a77bbc083c3fccfff9
Claudio Altamura <claudio@claudioaltamura.de>
2022-04-27 12:13:03 +0200

    chore: housekeeping

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+[![Build Status](https://travis-ci.com/claudioaltamura/java-jsonassert.svg?branch=master)](https://travis-ci.com/claudioaltamura/java-jsonassert)
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
 # java-jsonassert
 testing with json in java
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     java
     eclipse
     idea
-    id("com.diffplug.spotless") version "5.12.4"
-    id("com.github.ben-manes.versions") version "0.38.0"
+    id("com.diffplug.spotless") version "6.5.0"
+    id("com.github.ben-manes.versions") version "0.42.0"
 }
 
 repositories {
@@ -13,19 +13,14 @@ repositories {
 }
 
 dependencies {
-    implementation("com.fasterxml.jackson.core:jackson-core:2.12.3")
-    implementation("com.fasterxml.jackson.core:jackson-annotations:2.12.3")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.3")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.13.2")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.2")
 
     testImplementation("org.skyscreamer:jsonassert:1.5.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.1")
-    testImplementation("org.assertj:assertj-core:3.19.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.1")
-}
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_13
-    targetCompatibility = JavaVersion.VERSION_13
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testImplementation("org.assertj:assertj-core:3.22.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
 spotless {
